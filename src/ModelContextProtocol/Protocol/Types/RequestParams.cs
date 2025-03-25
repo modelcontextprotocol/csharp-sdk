a487b441a7b1f89c36d6c2f696af5eddbe562b0c
Aaron Powell <me@aaron-powell.com>
2025-03-26 10:01:15 +1100

    Adding a base class for request params that exposes the progressToken
    This aligns more with the type structure see in the other SDK's (eg: https://github.com/modelcontextprotocol/python-sdk/blob/main/src/mcp/types.py#L41) while also ensuring they are exposing the JSONRPCRequest (https://github.com/modelcontextprotocol/specification/blob/main/schema/2024-11-05/schema.json#L771-L806) data.
    
    Storing progressToken as an object as it could be an int or string (per https://github.com/modelcontextprotocol/specification/blob/main/schema/2024-11-05/schema.json#L1268-L1274).
    
    Fixes #67

---
+++ src/ModelContextProtocol/Protocol/Types/RequestParams.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ModelContextProtocol.Protocol.Types;
+
+/// <summary>
+/// Base class for all request parameters.
+/// <see href="https://github.com/modelcontextprotocol/specification/blob/main/schema/2024-11-05/schema.json#L771-L806">See the schema for details</see>
+/// </summary>
+public abstract class RequestParams
+{
+    /// <summary>
+    /// Metadata related to the tool invocation.
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("_meta")]
+    public RequestParamsMetadata? Meta { get; init; }
+}
---
+++ src/ModelContextProtocol/Protocol/Types/RequestParamsMetadata.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace ModelContextProtocol.Protocol.Types;
+
+/// <summary>
+/// Metadata related to the request.
+/// </summary>
+public class RequestParamsMetadata
+{
+    /// <summary>
+    /// If specified, the caller is requesting out-of-band progress notifications for this request (as represented by notifications/progress). The value of this parameter is an opaque token that will be attached to any subsequent notifications. The receiver is not obligated to provide these notifications.
+    /// </summary>
+    [System.Text.Json.Serialization.JsonPropertyName("progressToken")]
+    public object ProgressToken { get; set; } = default!;
+}
