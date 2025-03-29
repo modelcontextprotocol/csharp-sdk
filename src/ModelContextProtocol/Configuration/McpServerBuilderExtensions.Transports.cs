1f51437a330e23fbd4ad1f2af49cda8290f5e425
Wenzel, Toni <toni.wenzel@chg-meridian.com>
2025-03-29 17:29:37 +0100

    Add InMemoryTransport

--- src/ModelContextProtocol/Configuration/McpServerBuilderExtensions.Transports.cs
+++ src/ModelContextProtocol/Configuration/McpServerBuilderExtensions.Transports.cs
@@ -1,8 +1,10 @@
-﻿using ModelContextProtocol.Configuration;
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.Extensions.DependencyInjection;
+using ModelContextProtocol.Configuration;
 using ModelContextProtocol.Hosting;
+using ModelContextProtocol.Protocol.Messages;
 using ModelContextProtocol.Protocol.Transport;
 using ModelContextProtocol.Utils;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace ModelContextProtocol;
 
@@ -17,11 +19,7 @@ public static partial class McpServerBuilderExtensions
     /// <param name="builder">The builder instance.</param>
     public static IMcpServerBuilder WithStdioServerTransport(this IMcpServerBuilder builder)
     {
-        Throw.IfNull(builder);
-
-        builder.Services.AddSingleton<IServerTransport, StdioServerTransport>();
-        builder.Services.AddHostedService<McpServerHostedService>();
-        return builder;
+        return builder.WithServerTransport<StdioServerTransport>();
     }
 
     /// <summary>
@@ -30,9 +28,57 @@ public static partial class McpServerBuilderExtensions
     /// <param name="builder">The builder instance.</param>
     public static IMcpServerBuilder WithHttpListenerSseServerTransport(this IMcpServerBuilder builder)
     {
+        return builder.WithServerTransport<HttpListenerSseServerTransport>();
+    }
+
+    /// <summary>
+    /// Adds a server transport for in-memory communication.
+    /// </summary>
+    /// <param name="builder">The builder instance.</param>
+    public static IMcpServerBuilder WithInMemoryServerTransport(this IMcpServerBuilder builder)
+    {
+        return builder.WithServerTransport<InMemoryServerTransport>();
+    }
+
+    /// <summary>
+    /// Adds a server transport for in-memory communication.
+    /// </summary>
+    /// <param name="builder">The builder instance.</param>
+    /// <param name="handleMessageDelegate">Delegate to handle messages.</param>
+    public static IMcpServerBuilder WithInMemoryServerTransport(this IMcpServerBuilder builder, Func<IJsonRpcMessage, CancellationToken, Task<IJsonRpcMessage?>> handleMessageDelegate)
+    {
+        var transport = new InMemoryServerTransport
+        {
+            HandleMessage = handleMessageDelegate
+        };
+
+        return builder.WithServerTransport(transport);
+    }
+
+    /// <summary>
+    /// Adds a server transport for communication.
+    /// </summary>
+    /// <typeparam name="TTransport">The type of the server transport to use.</typeparam>
+    /// <param name="builder">The builder instance.</param>
+    public static IMcpServerBuilder WithServerTransport<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicConstructors)] TTransport>(this IMcpServerBuilder builder) where TTransport : class, IServerTransport
+    {
         Throw.IfNull(builder);
 
-        builder.Services.AddSingleton<IServerTransport, HttpListenerSseServerTransport>();
+        builder.Services.AddSingleton<IServerTransport, TTransport>();
+        builder.Services.AddHostedService<McpServerHostedService>();
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds a server transport for communication.
+    /// </summary>
+    /// <param name="serverTransport">Instance of the server transport.</param>
+    /// <param name="builder">The builder instance.</param>
+    public static IMcpServerBuilder WithServerTransport(this IMcpServerBuilder builder, IServerTransport serverTransport)
+    {
+        Throw.IfNull(builder);
+
+        builder.Services.AddSingleton<IServerTransport>(serverTransport);
         builder.Services.AddHostedService<McpServerHostedService>();
         return builder;
     }
