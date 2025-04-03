380cd6a248b8bcb5383d747a99c12b4c76071cb3
Andy Gocke <andy@commentout.net>
2025-04-03 10:47:03 -0700

    Revert changes to remove APIs

---
+++ samples/QuickstartWeatherServer/Tools/HttpClientExt.cs
@@ -0,0 +1,14 @@
+
+using System.Text.Json;
+
+namespace ModelContextProtocol;
+
+internal static class HttpClientExt
+{
+    public static async Task<JsonDocument> ReadJsonDocument(this HttpClient client, string requestUri)
+    {
+        using var response = await client.GetAsync(requestUri);
+        response.EnsureSuccessStatusCode();
+        return await JsonDocument.ParseAsync(await response.Content.ReadAsStreamAsync());
+    }
+}
