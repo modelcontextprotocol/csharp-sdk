e0d508ad72701fe16ca9f5454b38dcab8454c7ee
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-02 23:19:08 -0700

    Fixed default options values for McpServerTcpTransportOptions

--- src/ModelContextProtocol/Server/McpServerTcpTransportOptions.cs
+++ src/ModelContextProtocol/Server/McpServerTcpTransportOptions.cs
@@ -10,19 +10,10 @@ public class McpServerTcpTransportOptions
     /// <summary>
     /// The TCP port to listen on.
     /// </summary>
-    public required int Port { get; set; }
+    public required int Port { get; set; } = 60606;
 
     /// <summary>
     /// The TCP host to listen on. This is typically the IP address of the server. If not specified, the server will listen on all available network interfaces.
     /// </summary>
-    public required IPAddress IpAddress { get; set; }
-
-    /// <summary>
-    /// Default options for the TCP server transport.
-    /// </summary>
-    public static McpServerTcpTransportOptions Default => new McpServerTcpTransportOptions
-    {
-        Port = 60606,
-        IpAddress = IPAddress.Any
-    };
+    public required IPAddress IpAddress { get; set; } = IPAddress.Any;
 }
