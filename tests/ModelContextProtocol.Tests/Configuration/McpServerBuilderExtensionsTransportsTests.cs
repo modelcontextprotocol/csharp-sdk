7008e61b7be858e70af8e1ae800c1ab1a6153941
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-04-02 23:15:16 -0700

    Update McpServerBuilderExtensionsTransportsTests.cs

--- tests/ModelContextProtocol.Tests/Configuration/McpServerBuilderExtensionsTransportsTests.cs
+++ tests/ModelContextProtocol.Tests/Configuration/McpServerBuilderExtensionsTransportsTests.cs
@@ -1,6 +1,7 @@
 ﻿using ModelContextProtocol.Protocol.Transport;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using System.Net;
 
 namespace ModelContextProtocol.Tests.Configuration;
 
@@ -27,6 +28,23 @@ public class McpServerBuilderExtensionsTransportsTests
         builder.SetupGet(b => b.Services).Returns(services);
 
         builder.Object.WithTcpServerTransport();
+
+        var transportType = services.FirstOrDefault(s => s.ServiceType == typeof(ITransport));
+        Assert.NotNull(transportType);
+        Assert.Equal(typeof(TcpServerTransport), transportType.ImplementationType);
+    }
+    [Fact]
+    public void WithTcpServerTransport_Sets_Transport_Options()
+    {
+        var services = new ServiceCollection();
+        var builder = new Mock<IMcpServerBuilder>();
+        builder.SetupGet(b => b.Services).Returns(services);
+
+        builder.Object.WithTcpServerTransport(options =>
+        {
+            options.Port = 12345;
+            options.IpAddress = IPAddress.Parse("127.0.0.1");
+        });
 
         var transportType = services.FirstOrDefault(s => s.ServiceType == typeof(ITransport));
         Assert.NotNull(transportType);
