e6a591bf5119e3303123d3afe1270ffb26043978
Stephen Halter <halter73@gmail.com>
2025-04-07 20:50:25 -0700

    Fix StdioServerTransport.DisposeAsync hang
    - It's not just Unix, it's all platforms that can hang
    - Windows doesn't hang on Ctrl-C because that triggers
      the end of the Stream, but that doesn't help in IHostApplicationLifetime.StopAsync() scenarios
    
    https://github.com/dotnet/runtime/blob/4e9627e311806330be72b7f7d3660be699878ebd/src/libraries/System.Console/src/System/ConsolePal.Unix.ConsoleStream.cs#L13
    https://github.com/dotnet/runtime/blob/4e9627e311806330be72b7f7d3660be699878ebd/src/libraries/System.Console/src/System/ConsolePal.Windows.cs#L1149

---
+++ tests/ModelContextProtocol.Tests/StdioServerIntegrationTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace ModelContextProtocol.Tests;
+
+public class StdioServerIntegrationTests
+{
+    public static bool CanSendSigInt { get; } = RuntimeInformation.IsOSPlatform(OSPlatform.Linux) || RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+    internal const int SIGINT = 2;
+
+    [Fact(Skip = "Platform not supported by this test.", SkipUnless = nameof(CanSendSigInt))]
+    public async Task SigInt_DisposesTestServerWithHosting_Gracefully()
+    {
+        using var process = new Process
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = "TestServerWithHosting.dll",
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+
+        process.Start();
+
+        // I considered writing a similar test for windows using Ctrl-C, then saw that dotnet watch doesn't even send a Ctrl-C
+        // signal because it's such a pain without support CREATE_NEW_PROCESS_GROUP in System.Diagnostics.Process.
+        // https://github.com/dotnet/sdk/blob/43b1c12e3362098a23ca1018503eb56516840b6a/src/BuiltInTools/dotnet-watch/Internal/ProcessRunner.cs#L277-L303
+        // https://github.com/dotnet/runtime/issues/109432, https://github.com/dotnet/runtime/issues/44944
+        Assert.Equal(0, kill(process.Id, SIGINT));
+
+        using var shutdownCts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+        await process.WaitForExitAsync(shutdownCts.Token);
+
+        Assert.True(process.HasExited);
+        Assert.Equal(130, process.ExitCode);
+    }
+
+    [DllImport("libc", SetLastError = true)]
+    internal static extern int kill(int pid, int sig);
+}
