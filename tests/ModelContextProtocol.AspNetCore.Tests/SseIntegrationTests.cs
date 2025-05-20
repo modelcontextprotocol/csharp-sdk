1d05999d54fc10973abd7a7e0e08aaa0dc569be2
Shoham Shitrit <sshitrit@microsoft.com>
2025-05-20 15:20:22 -0400

    fix unused using

--- tests/ModelContextProtocol.AspNetCore.Tests/SseIntegrationTests.cs
+++ tests/ModelContextProtocol.AspNetCore.Tests/SseIntegrationTests.cs
@@ -8,7 +8,6 @@ using ModelContextProtocol.AspNetCore.Tests.Utils;
 using ModelContextProtocol.Client;
 using ModelContextProtocol.Protocol;
 using ModelContextProtocol.Server;
-using ModelContextProtocol.Utils.Json;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
