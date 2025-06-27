c32c6c577d205e8b436b857dd4ebc54081bd65c9
Ludovit Scholtz <ludko2@gmail.com>
2025-06-27 21:13:37 +0200

    Get session id sample code #564

---
+++ samples/GetSessionId/GetSessionIdTool.cs
@@ -0,0 +1,18 @@
+﻿using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+[McpServerToolType]
+public class GetSessionIdTool
+{
+    private readonly IMcpServer _mcpServer;
+    public GetSessionIdTool(IMcpServer mcpServer)
+    {
+        _mcpServer = mcpServer;
+    }
+
+    [McpServerTool(Name = "get_session"), Description("Returns current session id")]
+    public async Task<string?> GetSession()
+    {
+        return _mcpServer.SessionId;
+    }
+}
