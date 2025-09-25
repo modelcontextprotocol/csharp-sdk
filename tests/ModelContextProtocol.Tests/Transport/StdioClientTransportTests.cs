d7edf53b65a8807cc72360023d2ed7b862704364
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2025-09-25 14:37:32 +0300

    Tweak tests further

--- tests/ModelContextProtocol.Tests/Transport/StdioClientTransportTests.cs
+++ tests/ModelContextProtocol.Tests/Transport/StdioClientTransportTests.cs
@@ -16,14 +16,14 @@ public class StdioClientTransportTests(ITestOutputHelper testOutputHelper) : Log
         string id = Guid.NewGuid().ToString("N");
 
         StdioClientTransport transport = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
-            new(new() { Command = "cmd", Arguments = ["/c", $"timeout /t 2 /nobreak >nul & echo {id} >&2 & exit /b 1"] }, LoggerFactory) :
-            new(new() { Command = "sh", Arguments = ["-c", $"sleep 2s; echo {id} >&2; exit 1"] }, LoggerFactory);
+            new(new() { Command = "cmd", Arguments = ["/c", $"echo {id} >&2 & exit /b 1"] }, LoggerFactory) :
+            new(new() { Command = "sh", Arguments = ["-c", $"echo {id} >&2; exit 1"] }, LoggerFactory);
 
-        IOException e = await Assert.ThrowsAsync<IOException>(() => McpClient.CreateAsync(transport, loggerFactory: LoggerFactory, cancellationToken: TestContext.Current.CancellationToken));
-        Assert.Contains(id, e.ToString());
+        await Assert.ThrowsAsync<IOException>(() => McpClient.CreateAsync(transport, loggerFactory: LoggerFactory, cancellationToken: TestContext.Current.CancellationToken));
     }
 
-    [Fact(Skip = "Platform not supported by this test.", SkipUnless = nameof(IsStdErrCallbackSupported))]
+    // [Fact(Skip = "Platform not supported by this test.", SkipUnless = nameof(IsStdErrCallbackSupported))]
+    [Fact]
     public async Task CreateAsync_ValidProcessInvalidServer_StdErrCallbackInvoked()
     {
         string id = Guid.NewGuid().ToString("N");
@@ -41,8 +41,8 @@ public class StdioClientTransportTests(ITestOutputHelper testOutputHelper) : Log
         };
 
         StdioClientTransport transport = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ?
-            new(new() { Command = "cmd", Arguments = ["/c", $"timeout /t 2 /nobreak >nul & echo {id} >&2 & exit /b 1"], StandardErrorLines = stdErrCallback }, LoggerFactory) :
-            new(new() { Command = "sh", Arguments = ["-c", $"sleep 2s; echo {id} >&2; exit 1"], StandardErrorLines = stdErrCallback }, LoggerFactory);
+            new(new() { Command = "cmd", Arguments = ["/c", $"echo {id} >&2 & exit /b 1"], StandardErrorLines = stdErrCallback }, LoggerFactory) :
+            new(new() { Command = "sh", Arguments = ["-c", $"echo {id} >&2; exit 1"], StandardErrorLines = stdErrCallback }, LoggerFactory);
 
         await Assert.ThrowsAsync<IOException>(() => McpClient.CreateAsync(transport, loggerFactory: LoggerFactory, cancellationToken: TestContext.Current.CancellationToken));
 
