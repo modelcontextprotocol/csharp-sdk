9e8de5c64b333ac9c083db4bb1d8e367ce3fe5c9
Stephen Toub <stoub@microsoft.com>
2025-10-16 21:24:16 -0400

    Change approach and add lots of tests

--- src/ModelContextProtocol.Core/Server/AIFunctionMcpServerTool.cs
+++ src/ModelContextProtocol.Core/Server/AIFunctionMcpServerTool.cs
@@ -369,7 +369,7 @@ internal sealed partial class AIFunctionMcpServerTool : McpServerTool
         {
             if (meta?.ContainsKey(attr.Name) is not true)
             {
-                (meta ??= [])[attr.Name] = JsonNode.Parse(attr.Value);
+                (meta ??= [])[attr.Name] = JsonNode.Parse(attr.JsonValue);
             }
         }
 
--- src/ModelContextProtocol.Core/Server/McpMetaAttribute.cs
+++ src/ModelContextProtocol.Core/Server/McpMetaAttribute.cs
@@ -1,5 +1,7 @@
 using ModelContextProtocol.Protocol;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace ModelContextProtocol.Server;
 
@@ -20,28 +22,52 @@ namespace ModelContextProtocol.Server;
 /// Metadata can be used to attach additional information to primitives, such as model preferences,
 /// version information, or other custom data that should be communicated to MCP clients.
 /// </para>
+/// </remarks>
 /// <example>
 /// <code>
 /// [McpServerTool]
-/// [McpMeta("model", "\"gpt-4o\"")]
-/// [McpMeta("version", "\"1.0\"")]
-/// [McpMeta("priority", "5")]
+/// [McpMeta("model", "gpt-4o")]
+/// [McpMeta("version", "1.0")]
+/// [McpMeta("priority", 5.0)]
+/// [McpMeta("isBeta", true)]
+/// [McpMeta("tags", JsonValue = """["a","b"]""")]
 /// public string MyTool(string input) => $"Processed: {input}";
 /// </code>
 /// </example>
-/// </remarks>
 [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
 public sealed class McpMetaAttribute : Attribute
 {
     /// <summary>
-    /// Initializes a new instance of the <see cref="McpMetaAttribute"/> class.
+    /// Initializes a new instance of the <see cref="McpMetaAttribute"/> class with a string value.
     /// </summary>
     /// <param name="name">The name (key) of the metadata entry.</param>
-    /// <param name="value">The value of the metadata entry as a JSON string. This must be well-formed JSON.</param>
-    public McpMetaAttribute(string name, [StringSyntax(StringSyntaxAttribute.Json)] string value)
+    /// <param name="value">The string value of the metadata entry. If null, the value will be serialized as JSON null.</param>
+    public McpMetaAttribute(string name, string? value = null)
     {
         Name = name;
-        Value = value;
+        JsonValue = value is null ? "null" : JsonSerializer.Serialize(value, McpJsonUtilities.DefaultOptions.GetTypeInfo(typeof(string)));
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="McpMetaAttribute"/> class with a double value.
+    /// </summary>
+    /// <param name="name">The name (key) of the metadata entry.</param>
+    /// <param name="value">The double value of the metadata entry.</param>
+    public McpMetaAttribute(string name, double value)
+    {
+        Name = name;
+        JsonValue = JsonSerializer.Serialize(value, McpJsonUtilities.DefaultOptions.GetTypeInfo(typeof(double)));
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="McpMetaAttribute"/> class with a boolean value.
+    /// </summary>
+    /// <param name="name">The name (key) of the metadata entry.</param>
+    /// <param name="value">The boolean value of the metadata entry.</param>
+    public McpMetaAttribute(string name, bool value)
+    {
+        Name = name;
+        JsonValue = value ? "true" : "false";
     }
 
     /// <summary>
@@ -54,13 +80,16 @@ public sealed class McpMetaAttribute : Attribute
     public string Name { get; }
 
     /// <summary>
-    /// Gets the value of the metadata entry as a JSON string.
+    /// Gets or sets the value of the metadata entry as a JSON string.
     /// </summary>
     /// <remarks>
     /// <para>
-    /// This value must be well-formed JSON. It will be parsed and added to the metadata JsonObject.
+    /// This value must be well-formed JSON. It will be parsed and added to the metadata <see cref="JsonObject"/>.
     /// Simple values can be represented as JSON literals like <c>"\"my-string\""</c>, <c>"123"</c>, 
     /// <c>"true"</c>, etc. Complex structures can be represented as JSON objects or arrays.
+    /// </para>
+    /// <para>
+    /// Setting this property will override any value provided via the constructor.
     /// </para>
     /// <para>
     /// For programmatic scenarios where you want to construct complex metadata without dealing with
@@ -70,5 +99,5 @@ public sealed class McpMetaAttribute : Attribute
     /// </para>
     /// </remarks>
     [StringSyntax(StringSyntaxAttribute.Json)]
-    public string Value { get; }
+    public string JsonValue { get; set; }
 }
