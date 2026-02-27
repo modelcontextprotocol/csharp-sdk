4584029dcde54cd12b4cb882abd3ee785b9db6e3
Stephen Toub <stoub@microsoft.com>
2026-02-27 14:51:05 -0500

    Add test comment to DefaultMcpMessageFilterBuilder
    Added a comment indicating that this code is for testing purposes and should not be merged.

--- src/ModelContextProtocol/DefaultMcpMessageFilterBuilder.cs
+++ src/ModelContextProtocol/DefaultMcpMessageFilterBuilder.cs
@@ -2,5 +2,6 @@ namespace Microsoft.Extensions.DependencyInjection;
 
 internal sealed class DefaultMcpMessageFilterBuilder(IMcpServerBuilder serverBuilder) : IMcpMessageFilterBuilder
 {
+    // Test. Do not merge.
     public IServiceCollection Services { get; } = serverBuilder.Services;
 }
